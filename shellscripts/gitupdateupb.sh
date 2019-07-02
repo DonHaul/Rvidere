9c45d11bf1f28ceff6ce9c5fcf5667426e5d505e
mncosta <migascosta@hotmail.com>
2019-07-02 16:32:22 +0100

    things

--- shellscripts/gitupdateupb.sh
+++ shellscripts/gitupdateupb.sh
@@ -3,5 +3,7 @@ ssh sipg@$1 << EOF
 cd ./UpBoard-FeedBot/
 pwd
 git pull
+echo PULLADD
 bash shellscripts/historic.sh PULL
+echo PULL DONE
 EOF
--- shellscripts/historic.sh
+++ shellscripts/historic.sh
@@ -12,6 +12,7 @@ data=$(date +"%I:%M:%S %d/%m/%Y")
 
 #joins date and argument
 towrite="$1\t$data"
+echo $towrite
 
 #adds to begining of file
-sed -i "1i $towrite" $mydir/../historic.log
+sed -i "1i\ $towrite" $mydir/../historic.log
