743c1b8989db8d6e343c19b19536a3d5925dd158
mncosta <migascosta@hotmail.com>
2019-07-03 16:36:03 +0100

    new gitignore added

--- catkin_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gmock
-# Build directory: /home/ramiro/Desktop/UpBoard-FeedBot/catkin_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
