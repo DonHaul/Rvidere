69c4695944ff9c1ebf2bf91faf447fe5a1a1605f
mncosta <migascosta@hotmail.com>
2019-07-03 20:19:31 +0100

    -

--- shellscripts/setup.sh
+++ shellscripts/setup.sh
@@ -1,7 +1,8 @@
 #this is called from the bashrc
-
+echo "MY NAME IS JEFF"
 source UpBoard-FeedBot/catkin_ws/devel/setup.bash
-
+lol=hostname
+echo $lol
 export ROS_HOSTNAME=hostname
 export ROS_MASTER_URI=http://metro_server01:11311
 
