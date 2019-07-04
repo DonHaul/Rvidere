d4fb3b91731bc07fc06b8eafedfb8d1c098f3246
mncosta <ramiro.animus@gmail.com>
2019-07-04 15:03:42 +0100

    -

--- shellscripts/history.sh
+++ shellscripts/history.sh
@@ -4,15 +4,21 @@ mydir=$(dirname "$0")
 #goes to parent directory
 cd "$mydir/.."
 
+
+echo "AAAAAAAA"
+
 #creates if it doesnt exist yet, the historic.log file
 touch history.log
 
 #formats date
 data=$(date +"%T %d/%m/%Y")
 
+echo "BBBBB"
 #joins date and argument
 towrite="$1\t$data"
 
 
 #adds to begining of file -e is for output tabs
 echo -e $towrite | cat - history.log > temp && mv temp history.log
+
+echo "AUIOHDIOHSIODHIO"
