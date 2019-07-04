f8dfc7566eea5f3bac312818f032290067cf6a77
mncosta <ramiro.animus@gmail.com>
2019-07-04 11:33:38 +0100

    metro03

--- shellscripts/lookandstart.sh
+++ shellscripts/lookandstart.sh
@@ -1,12 +1,7 @@
 echo "scriptrunning"
+#source UpBoard-FeedBot/catkin_ws/devel/setup.bash
 bash UpBoard-FeedBot/shellscripts/history.sh START
-echo what2
-var=lol
-echo $var
-echo whatnnn
 line=$(grep metro03 UpBoard-FeedBot/sysconfig.txt)
-echo what3
 camname=$(echo $line | cut -d " " -f 3)
-echo what
-echo "what"
-echo $camname
+roslaunch realsense_camera  zr300_nodelet_rgbd.launch
+#echo $camname
--- shellscripts/startupb.sh
+++ shellscripts/startupb.sh
@@ -1,14 +1,5 @@
 ssh sipg@$1 << EOF
 source UpBoard-FeedBot/catkin_ws/devel/setup.bash
-
-bash UpBoard-FeedBot/shellscripts/history.sh START
-line=$(grep metro03 UpBoard-FeedBot/sysconfig.txt)
-camname=$(echo $line | cut -d " " -f 3)
-echo "what"
-echo $camname
-echo jeff
-#tmux new-session -d -s lol "echo hey"
-#tmux send-key -t lol "echo jeff"
-#tmux new-session -d -s ros "roslaunch multiple_realsense speedwagon.launch"
-#tmux new-session -d -s ros "roslaunch realsense_camera  zr300_nodelet_rgbd.launch"
+tmux new-session -d -s ros
+tmux send-keys -t "ros" 'bash UpBoard-FeedBot/shellscripts/lookandstart.sh' Enter
 EOF
