cbba37617fa1b73785575394f67dcbba05bc20b9
mncosta <ramiro.animus@gmail.com>
2019-07-04 11:29:06 +0100

    -

--- shellscripts/hello2.sh
+++ shellscripts/hello2.sh
@@ -1,1 +1,12 @@
 echo "scriptrunning"
+bash UpBoard-FeedBot/shellscripts/history.sh START
+echo what2
+var=lol
+echo $var
+echo whatnnn
+line=$(grep metro03 UpBoard-FeedBot/sysconfig.txt)
+echo what3
+camname=$(echo $line | cut -d " " -f 3)
+echo what
+echo "what"
+echo $camname
