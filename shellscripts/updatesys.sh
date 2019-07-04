224aab6cc81be318589d04336fe90bcbeb899d94
mncosta <ramiro.animus@gmail.com>
2019-07-04 12:00:50 +0100

    -

--- shellscripts/updatesys.sh
+++ shellscripts/updatesys.sh
@@ -1,5 +1,4 @@
 git add .
-
 if [ -z "$1" ]
 then
     echo "put commit message next time!"
@@ -8,8 +7,26 @@ else
       git commit -m $1
 fi
 git push
+gets script directory
+mydir=$(dirname "$0")
 
-
-cams=echo *  | head -n1 | cut -d " " -f1
+#get first word of each line
+cams=$(cut -d' ' -f1 $mydir/../sysconfig.txt)
 echo $cams
 echo jeff
+
+count=0
+for one_thing in $cams; do
+   
+
+    if [ "$count" != "0" ]; then
+     echo $one_thing
+     echo "not zero"
+     tmux new-session -d "bash $mydir/gitupdateupb.sh $one_thing"
+    fi
+
+    ((count++))
+    echo $count
+done
+
+   
