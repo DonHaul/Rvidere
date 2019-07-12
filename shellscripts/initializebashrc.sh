957d9da7f950052fc8ddcf99b6c58dc4d7da4b9a
mncosta <ramiro.animus@gmail.com>
2019-07-12 20:02:05 +0100

    -

--- shellscripts/initializebashrc.sh
+++ shellscripts/initializebashrc.sh
@@ -9,7 +9,6 @@ else
 sed -i '1s/^/source /home/sipg/UpBoard-FeedBot/shellscripts/setup.sh'  ~/.bashrc
 sed -i '1s/^/source /opt/ros/kinetic/setup.bash' ~/.bashrc
 echo "FILEs Have Been Added"
-
 #rerun bash
 source ~/.bashrc
 fi
