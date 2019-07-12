4763ccacbe8b685a5b9d4c30a653099d2bf3e436
mncosta <ramiro.animus@gmail.com>
2019-07-12 20:09:08 +0100

    -

--- shellscripts/initializebashrc.sh
+++ shellscripts/initializebashrc.sh
@@ -6,9 +6,10 @@ if grep -q "/home/sipg/UpBoard-FeedBot" ~/.bashrc
 then 
 echo "FILE ALREADY THERE"
 else
-
-sed -i '1i\'"/source /home/sipg/UpBoard-FeedBot/shellscripts/setup.sh" ~/.bashrc
-sed -i '1i\'"/source /opt/ros/kinetic/setup.bash" ~/.bashrc
+$ola="/source /home/sipg/UpBoard-FeedBot/shellscripts/setup.sh"
+$ola2="/source /opt/ros/kinetic/setup.bash"
+sed -i '1i\'"$ola" ~/.bashrc
+sed -i '1i\'"$ola2" ~/.bashrc
 
 echo "FILEs Have Been Added"
 #rerun bash
