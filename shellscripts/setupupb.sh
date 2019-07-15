450c464e3c11f929a5c88115cba890737439ad98
mncosta <ramiro.animus@gmail.com>
2019-07-15 19:25:15 +0100

    -

--- shellscripts/setupupb.sh
+++ shellscripts/setupupb.sh
@@ -4,11 +4,10 @@ if grep -q "/home/sipg/UpBoard-FeedBot" ~/.bashrc
 then 
 echo "FILE ALREADY THERE"
 else
-$ola="source /home/sipg/UpBoard-FeedBot/shellscripts/setup.sh"
-$ola2="source /opt/ros/kinetic/setup.bash"
+ola="source /home/sipg/UpBoard-FeedBot/shellscripts/setup.sh"
+ola2="source /opt/ros/kinetic/setup.bash"
 sed -i '1i\'"$ola" ~/.bashrc
 sed -i '1i\'"$ola2" ~/.bashrc
-
 echo "FILEs Have Been Added"
 #rerun bash
 fi
