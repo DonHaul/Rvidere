607a79e9dee593efeb9d1586ebaff9d9bc59cb6e
mncosta <ramiro.animus@gmail.com>
2019-09-06 18:20:08 +0100

    -

--- shellscripts/lookandstart.sh
+++ shellscripts/lookandstart.sh
@@ -2,6 +2,7 @@ echo "scriptrunning"
 source UpBoard-FeedBot/catkin_ws/devel/setup.bash
 bash UpBoard-FeedBot/shellscripts/history.sh START
 line=$(grep $(hostname) UpBoard-FeedBot/sysconfig.txt)
+echo $line
 camname=$(echo $line | cut -d " " -f 3)
 #roslaunch realsense_camera  zr300_nodelet_rgbd.launch
 roslaunch multiple_realsense $camname.launch
