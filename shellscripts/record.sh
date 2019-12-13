949ecd6f651be631bfc66c44fe9c483c9dd8d67c
mncosta <ramiro.animus@gmail.com>
2019-12-13 17:08:11 +0000

    recording added

---
+++ shellscripts/record.sh
@@ -0,0 +1,3 @@
+rosbag record --split --size=4096 -b 0 /speedwagon/thefutureisnow/rgb /speedwagon/thefutureisnow/depth /emperorcrimson/thefutureisnow/rgb /emperorcrimson/thefutureisnow/depth /killerqueen/thefutureisnow/rgb /mista/thefutureisnow/depth /diavolo/thefutureisnow/rgb /diavolo/thefutureisnow/depth
+
+
